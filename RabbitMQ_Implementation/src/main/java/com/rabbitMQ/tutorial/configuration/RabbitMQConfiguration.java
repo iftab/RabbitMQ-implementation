bb8018d148fba5bd41365751c5ec5ec4114eb868
Munsi Md Iftabuddin <iftabuddin123@gmail.com>
2024-09-19 10:11:13 +0530

    Commit changes for JSON message<iftabuddin123@gmail.com>

--- RabbitMQ_Implementation/src/main/java/com/rabbitMQ/tutorial/configuration/RabbitMQConfiguration.java
+++ RabbitMQ_Implementation/src/main/java/com/rabbitMQ/tutorial/configuration/RabbitMQConfiguration.java
@@ -1,9 +1,14 @@
 package com.rabbitMQ.tutorial.configuration;
 
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,17 +21,29 @@ public class RabbitMQConfiguration {
 	@Value("${rabbitmq.queue.name}")
 	private String queue;
 	
+	@Value("${rabbitmq.queue.json.queue-name}")
+	private String jsonqueue;
+	
 	@Value("${rabbitmq.queue.exchange-name}")
 	private String exchange_name;
 	
 	@Value("${rabbitmq.queue.routing_key}")
 	private String routingKey;
 	
+	@Value("${rabbitmq.queue.json.routing_key}")
+	private String jsonroutingKey;
+	
 	// Spring Bean for rabbitMQ Queue
 	@Bean
 	public Queue queue() {
 		return new Queue(queue);
 	}
+	
+	@Bean
+	public Queue jsonQueue() {
+		return new Queue(jsonqueue);
+	}
+	
 	
 	// Creating exchange bean
 	@Bean
@@ -41,6 +58,26 @@ public class RabbitMQConfiguration {
 				.bind(queue())
 				.to(exchange())
 				.with(routingKey);
+	}
+	
+	@Bean
+	public Binding jsonbinding() {
+		return BindingBuilder
+				.bind(jsonQueue())
+				.to(exchange())
+				.with(jsonroutingKey);
+	}
+	
+	@Bean
+	public MessageConverter coverter() {
+		return new Jackson2JsonMessageConverter();
+	}
+	
+	@Bean
+	public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
+	   RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+	   rabbitTemplate.setMessageConverter(coverter());
+	   return rabbitTemplate;
 	}
 	
 	// Spring Auto-configuration will automatically create bean below infrastructure beans.
